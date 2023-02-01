18c2a35399496c77f83faeb338db463a70e92cb8
Wilsker <jthomasw@cern.ch>
2023-02-01 17:21:39 +0100

    adding condor submission scripts

---
+++ truth_reco/scalar_reco.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /afs/cern.ch/work/j/jthomasw/private/NTU/ExtraYukawa/HiggsReco/myvenv/bin/activate
+python3 /afs/cern.ch/work/j/jthomasw/private/NTU/ExtraYukawa/HiggsReco/truth_reco/scalar_reco.py -n 100000 -p 1
